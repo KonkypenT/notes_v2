7cea93f48e87b319136bb36a19e8f4d8553b6af1
Лихолетов Дмитрий <liholetovds@it2g.ru>
2022-07-20 07:47:35 +0700

    build ios

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
+const host = 'http://89.223.126.211:3000';
+
 export const environment = {
   production: true,
-  baseUrl: 'notes',
+  host,
+  baseUrl: `${host}/notes`,
   dictionaryKey: 'ruesnx6333',
   mapKey: '614c04d1-04ce-446b-bfd7-654664774508',
+  envName: 'prod'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: false,
+  host: '',
   baseUrl: 'notes',
   dictionaryKey: 'ruesnx6333',
   mapKey: '614c04d1-04ce-446b-bfd7-654664774508',
+  envName: 'local'
 };
