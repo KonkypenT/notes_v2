768884d42270f3708547744c8da3ae59870f32b0
Лихолетов Дмитрий <liholetovds@it2g.ru>
2022-08-02 17:31:33 +0700

    Попытка сборки под android

--- frontend/src/environments/environment.stage.ts
+++ frontend/src/environments/environment.stage.ts
@@ -6,5 +6,5 @@ export const environment = {
   baseUrl: `${host}/notes`,
   dictionaryKey: 'ruesnx6333',
   mapKey: '614c04d1-04ce-446b-bfd7-654664774508',
-  envName: 'stage'
+  envName: 'stage',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   baseUrl: 'notes',
   dictionaryKey: 'ruesnx6333',
   mapKey: '614c04d1-04ce-446b-bfd7-654664774508',
-  envName: 'local'
+  envName: 'local',
 };
